b1c668396d7722b1def20a72f7d17361829d5b7b
abisheek23 <abisheek2397@gmail.com>
2024-08-08 11:33:54 +0530

    table

--- samp.py
+++ samp.py
@@ -1,8 +1,12 @@
-n=5
-for i in range(n):
-        for j in range(n - i - 1):
-            print(" ", end="")
-        for k in range( 2* i-1 ):
-            print("*", end="")
-        print()
+# n=5
+# for i in range(n):
+#         for j in range(n - i - 1):
+#             print(" ", end="")
+#         for k in range( 2* i-1 ):
+#             print("*", end="")
+#         print()
 
+l=[1,3,4,5,6,]
+for i in l:
+      print('*'*i)
+      

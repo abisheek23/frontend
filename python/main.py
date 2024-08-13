caa35e23f61c01ea180fa3af9f0d49b01de2c3e1
abisheek23 <abisheek2397@gmail.com>
2024-08-13 11:34:25 +0530

    css

---
+++ python/main.py
@@ -0,0 +1,19 @@
+from acc import *
+accounts = []
+
+while True:
+    print("1. Create account\n2. Login to account\n3. Exit")
+    choice = int(input('Enter your choice: '))
+
+    if choice == 1:
+        acc()
+
+    elif choice == 2:
+        log()
+
+    elif choice == 3:
+        print('Exiting...!')
+        break
+
+    else:
+        print('Invalid choice....!')
